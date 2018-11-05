f16499ec8384ddd33f9ceaaeaf29bb8e1c641ced
andrewnaeve <andrewnaeve@gmail.com>
2018-11-04 19:47:16 -0800

    add bio

---
+++ src/components/selected-works/PortfolioImage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import { media } from '../../utilities/style-utils';
+
+export default ({
+  image: {
+    childImageSharp: { fluid }
+  },
+  title,
+  description
+}) => (
+  <Wrapper>
+    <Img fluid={fluid} />
+    <TextContainer>
+      <Title> {title} </Title>
+      <Description> {description} </Description>
+    </TextContainer>
+  </Wrapper>
+);
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 900px;
+  width: 90%;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-direction: column;
+  color: black;
+  margin: 20px 0;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  font-style: italic;
+  ${media.m`
+    font-size: 2rem`};
+`;
+
+const Description = styled.div`
+  font-size: 1.2rem;
+  display: flex;
+  text-align: center;
+  width: 100%;
+  justify-content: center;
+  ${media.m`
+		font-size: 1.5rem`};
+`;
--- src/templates/selected-works.js
+++ src/templates/selected-works.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 import Layout from '../layouts';
 import styled from 'styled-components';
-import { media } from '../utilities/style-utils';
+import PortfolioImage from '../components/selected-works/PortfolioImage';
 
-export const SelectedWorksTemplate = ({
-  image: {
-    childImageSharp: { fluid }
-  },
-  title,
-  description
-}) => (
-  <Wrapper>
-    <Img fluid={fluid} />
-    <TextContainer>
-      <Title> {title} </Title>
-      <Description> {description} </Description>
-    </TextContainer>
-  </Wrapper>
-);
-
-const SelectedWorks = ({ data = {} }) => {
-  const [portfolio] = data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter.portfolio);
+const SelectedWorks = ({ data }) => {
+  const portfolio = data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter.portfolio[0]);
   return (
     <Layout>
       <Container>
         {portfolio.map(({ image, title, description }) => (
-          <SelectedWorksTemplate
-            key={image.id}
-            image={image}
-            title={title}
-            description={description}
-          />
+          <PortfolioImage key={image.id} image={image} title={title} description={description} />
         ))}
       </Container>
     </Layout>
@@ -44,46 +22,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  max-width: 900px;
-  width: 90%;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const TextContainer = styled.div`
-  display: flex;
-  font-family: 'Abel', sans-serif;
-  width: 100%;
-  justify-content: center;
-  color: black;
-  flex-direction: column;
-  margin-bottom: 20px;
-  margin-top: 20px;
-`;
-
-const Title = styled.div`
-  font-size: 1.5rem;
-  display: flex;
-  text-align: center;
-  width: 100%;
-  justify-content: center;
-  font-style: italic;
-  ${media.m`
-		font-size: 2rem`};
-`;
-
-const Description = styled.div`
-  font-size: 1.2rem;
-  display: flex;
-  text-align: center;
-  width: 100%;
-  justify-content: center;
-  ${media.m`
-		font-size: 1.5rem`};
 `;
 
 export default SelectedWorks;
@@ -100,7 +38,7 @@ export const SelectedWorksQuery = graphql`
               image {
                 id
                 childImageSharp {
-                  fluid(maxWidth: 1200) {
+                  fluid(maxWidth: 900) {
                     ...GatsbyImageSharpFluid_withWebp_noBase64
                   }
                 }
