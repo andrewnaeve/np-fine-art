aeeed2b46e7f48c58f08b3fc93f61bd544840c01
andrewnaeve <andrewnaeve@gmail.com>
2018-11-04 19:25:39 -0800

    portfolio section in

--- src/templates/selected-works.js
+++ src/templates/selected-works.js
@@ -1,49 +1,90 @@
 import React from 'react';
+import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 import Layout from '../layouts';
+import styled from 'styled-components';
+import { media } from '../utilities/style-utils';
 
-export const SelectedWorksTemplate = ({ image, title, description }) => (
-  <section className="section section--gradient">
-    <div className="container">
-      <div className="section">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="content">
-              <div
-                className="full-width-image-container margin-top-0"
-                style={{ backgroundImage: `url(${image})` }}
-              >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem'
-                  }}
-                >
-                  {title}
-                  {description}
-                </h2>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+export const SelectedWorksTemplate = ({
+  image: {
+    childImageSharp: { fluid }
+  },
+  title,
+  description
+}) => (
+  <Wrapper>
+    <Img fluid={fluid} />
+    <TextContainer>
+      <Title> {title} </Title>
+      <Description> {description} </Description>
+    </TextContainer>
+  </Wrapper>
 );
 
-const SelectedWorks = ({ data }) => {
-  console.log('data', data);
+const SelectedWorks = ({ data = {} }) => {
+  const [portfolio] = data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter.portfolio);
   return (
     <Layout>
-      {/* {portfolio.map(({ title, description, image }) => (
-        <SelectedWorksTemplate key={title} image={image} title={title} description={description} />
-      ))} */}
+      <Container>
+        {portfolio.map(({ image, title, description }) => (
+          <SelectedWorksTemplate
+            key={image.id}
+            image={image}
+            title={title}
+            description={description}
+          />
+        ))}
+      </Container>
     </Layout>
   );
 };
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 900px;
+  width: 90%;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  font-family: 'Abel', sans-serif;
+  width: 100%;
+  justify-content: center;
+  color: black;
+  flex-direction: column;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 1.5rem;
+  display: flex;
+  text-align: center;
+  width: 100%;
+  justify-content: center;
+  font-style: italic;
+  ${media.m`
+		font-size: 2rem`};
+`;
+
+const Description = styled.div`
+  font-size: 1.2rem;
+  display: flex;
+  text-align: center;
+  width: 100%;
+  justify-content: center;
+  ${media.m`
+		font-size: 1.5rem`};
+`;
 
 export default SelectedWorks;
 
@@ -60,7 +101,7 @@ export const SelectedWorksQuery = graphql`
                 id
                 childImageSharp {
                   fluid(maxWidth: 1200) {
-                    src
+                    ...GatsbyImageSharpFluid_withWebp_noBase64
                   }
                 }
               }
