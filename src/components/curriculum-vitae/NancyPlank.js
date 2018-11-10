75fdaebd8ed2bb79b677e942fa63973b541752c6
andrewnaeve <andrewnaeve@gmail.com>
2018-11-09 22:56:14 -0800

    animate profile pic

--- src/components/curriculum-vitae/NancyPlank.js
+++ src/components/curriculum-vitae/NancyPlank.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import AnimatedContainer from '../animation/AnimatedContainer';
 
 export default ({ fluid }) => (
   <ImageWrapper>
-    <StyledImage fluid={fluid} />
+    <AnimatedContainer>
+      {({ handleLoad, animatePosition }) =>
+        animatePosition(
+          <InnerWrapper>
+            <StyledImage fluid={fluid} onLoad={() => handleLoad()} />
+          </InnerWrapper>
+        )
+      }
+    </AnimatedContainer>
   </ImageWrapper>
 );
 
 const ImageWrapper = styled.div`
-  max-width: 90%;
-  width: 300px;
   margin-bottom: 20px;
+  margin-top: 10px;
 `;
 
 const StyledImage = styled(Img)`
   border-radius: 3px;
+`;
+
+const InnerWrapper = styled.div`
+  max-width: 100%;
+  width: 300px;
 `;
