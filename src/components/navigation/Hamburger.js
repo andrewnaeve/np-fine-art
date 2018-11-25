e372e9e745316ff5fad99e38a72cd9b8a320aee1
andrewnaeve <andrewnaeve@gmail.com>
2018-11-24 16:22:12 -0800

    more performant mobile menu

---
+++ src/components/navigation/Hamburger.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default ({ toggleMenu }) => (
+  <Hamburger onClick={toggleMenu}>
+    <Patty />
+    <Patty />
+    <Patty />
+  </Hamburger>
+);
+
+const Hamburger = styled.div`
+  display: flex;
+  position: absolute;
+  top: 10px;
+  left: 20px;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  flex-direction: column;
+  padding-top: 4px;
+  margin-bottom: 10px;
+  z-index: 1;
+`;
+
+const Patty = styled.div`
+  width: 35px;
+  height: 4px;
+  margin-bottom: 4px;
+  background: #cdcdcd;
+  z-index: 1;
+  border-radius: 3px;
+`;
--- src/components/navigation/MobileNav.js
+++ src/components/navigation/MobileNav.js
@@ -1,34 +1,78 @@
 import React, { Component } from 'react';
-import { Menu } from './NavMenu';
-import { Spring } from 'react-spring';
+import styled from 'styled-components';
+import Hamburger from './Hamburger';
+import { Link } from 'gatsby';
+import { media } from '../../utilities/style-utils';
 
-class MobileNav extends Component {
+class Mnav extends Component {
   state = {
     open: false
   };
-  _toggleDrawer = this._toggleDrawer.bind(this);
-
   render() {
-    let { open } = this.state;
-
-    const isOpen = {
-      x: open ? 10 : 290,
-      opacity: open ? 1 : 0
-    };
-
+    const { open } = this.state;
     return (
-      <Spring to={isOpen}>
-        {interpolatingStyle => (
-          <Menu interpolatingStyle={interpolatingStyle} toggleDrawer={this._toggleDrawer} />
-        )}
-      </Spring>
+      <Container open={open}>
+        <Hamburger toggleMenu={this.toggleMenu} />
+        <LinkWrapper>
+          <QuickLink to="/" onClick={this.toggleMenu} activeStyle={{ color: '#78D5E3' }}>
+            Home
+          </QuickLink>
+          <QuickLink to="/select-work" onClick={this.toggleMenu} activeStyle={{ color: '#78D5E3' }}>
+            Select Works
+          </QuickLink>
+          <QuickLink to="/biography" onClick={this.toggleMenu} activeStyle={{ color: '#78D5E3' }}>
+            Biography
+          </QuickLink>
+          <QuickLink
+            to="/curriculum-vitae"
+            onClick={this.toggleMenu}
+            activeStyle={{ color: '#78D5E3' }}
+          >
+            CV
+          </QuickLink>
+          <QuickLink to="/contact" onClick={this.toggleMenu} activeStyle={{ color: '#78D5E3' }}>
+            Contact
+          </QuickLink>
+        </LinkWrapper>
+      </Container>
     );
   }
-  _toggleDrawer() {
+  toggleMenu = () => {
     this.setState(({ open }) => ({
       open: !open
     }));
-  }
+  };
 }
 
-export default MobileNav;
+export default Mnav;
+
+const Container = styled.div`
+  ${media.m`
+		display: none;`};
+  position: fixed;
+  bottom: 0;
+  transform: ${({ open }) => (open ? 'none' : 'translateY(200px)')};
+  height: 260px;
+  width: 100%;
+  background: #fff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease-in-out;
+  will-change: transform;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 60px;
+`;
+
+const QuickLink = styled(Link)`
+  display: flex;
+  font-size: 1.75rem;
+  margin-bottom: 10px;
+  color: black;
+  text-decoration: none;
+`;
